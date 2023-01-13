0a3788f9f09d574d2f1eee8bb7e8dc53abafd914
Vaibhavi Kishor Patel <vakpatel@silo.luddy.indiana.edu>
2023-01-12 19:08:50 -0500

    adding hello command in xinu

---
+++ shell/xsh_hello.c
@@ -0,0 +1,13 @@
+#include <xinu.h>
+#include <string.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xsh_date - obtain and print the current month, day, year, and time
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_hello(int32 nargs, char *args[]) {
+
+ printf("Hello %s, Welcome to the world of Xinu!!",args[1]);
+
+}
